24663fce5f8d476edd5041b63a9273ad7593ff06
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-07-10 12:10:21 +0800

    u

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 source venv/bin/activate
 ./venv/bin/mkdocs build
- cd open-c3.github.io
- git reset 23caecfd2cb6e6db3e2716adf9118ac4ea8a562d
- git clean -fd
- rsync -av ../site/ ./
- git add .
- git commit -m u
- git push -f
- cd ..
- ./sync.sh
+cd open-c3.github.io
+git reset 23caecfd2cb6e6db3e2716adf9118ac4ea8a562d
+git clean -fd
+rsync -av ../site/ ./
+
+cp ../fix-assets.sh ./
+cp ../fix-assets-book.sh ./
+
+git add .
+git commit -m u
+git push -f
+cd ..
+./sync.sh
